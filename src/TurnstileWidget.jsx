a03e8c4f791ad402542d942f2a4f45596adf0281
Rajesh Warna <13366914+warnasweb@users.noreply.github.com>
2025-07-13 22:24:34 +1000

    Update TurnstileWidget.jsx

--- src/TurnstileWidget.jsx
+++ src/TurnstileWidget.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Turnstile from 'react-turnstile';
 
-const SITE_KEY = '0x4AAAAAABk7LacJYysXuPUb';
+const SITE_KEY = '<ADD TURNSTILE Secret here>'; // get the secrect from https://dash.cloudflare.com/sign-up?to=/:account/turnstile
+
 
 export default function TurnstileWidget({ onVerify }) {
   return (
